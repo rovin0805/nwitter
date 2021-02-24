e1a3923ad1bb872a328b747870b294dde47a0909
rovin0805 <qhdrn67@naver.com>
2021-02-24 23:02:20 +0900

    log out

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,15 @@
+import { authService } from "fbInstance";
 import React from "react";
+import { useHistory } from "react-router-dom";
 
-const Profile = () => <span>Profile</span>;
+const Profile = () => {
+  const history = useHistory();
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push("/");
+  };
+
+  return <button onClick={onLogOutClick}>Log out</button>;
+};
 
 export default Profile;
