6b372c9f7cd761451dfc176c23c6c91b39fd425d
rovin0805 <qhdrn67@naver.com>
2021-02-24 21:35:59 +0900

    email log in & sign up

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Router from "components/Router";
 import { authService } from "fbInstance";
 
 function App() {
-  console.log(authService.currentUser);
+  const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
 
   return (
     <>
-      <Router isLoggedIn={isLoggedIn} />
+      {init ? <Router isLoggedIn={isLoggedIn} /> : "Initializing..."}
       <footer>©{new Date().getFullYear()} Nwitter</footer>
     </>
   );
